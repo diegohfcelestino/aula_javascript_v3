9fab5356ed373b363c573c4e34afe10bbd468850
diegohfcelestino <diegohfcelestino@gmail.com>
2020-12-04 17:11:00 -0300

    Atualizando o curso de java

---
+++ js/main.js
@@ -0,0 +1,79 @@
+
+
+var d = new Date;
+alert(d);
+alert("Hoje é dia " + d.getDay() + " / " + d.getMonth() + " / " + d.getFullYear());
+alert("São " + d.getHours() + " horas");
+alert("e " + d.getMinutes() + " minutos");
+
+
+/*
+var count;
+for(count=0; count <= 10; count++){
+alert(count);
+console.log(count);
+}
+*/
+
+
+/*
+var count = 0;
+while (count <= 10){
+    console.log(count);
+    alert(count);
+    count++
+};
+*/
+
+
+/*
+var idade = prompt("Qual sua idade?");
+//var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+
+/*
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+/*
+var lista = ["maça", "pêra", "laranja"];
+lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//alert(lista[1]);
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+console.log(lista.join(" - "));
+*/
+
+/*
+var nome = "Diego Henrique Ferreira";
+var idade = 13;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome +" tem "+ idade + " anos ");
+//alert(idade + idade2);
+console.log(nome)
+console.log(idade+idade2)
+console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+*/
+
