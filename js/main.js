190941e0934fccc16102946c54daba2cd437e2b4
diegohfcelestino <diegohfcelestino@gmail.com>
2020-12-09 16:08:31 -0300

    Finalizado a aula

--- js/main.js
+++ js/main.js
@@ -1,11 +1,72 @@
 
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    //alert("Obrigado por clicar");
+}
 
+function redirecionar(){
+    window.open("https://www.linkedin.com/in/diego-ferreira-34b6348b/");
+   // window.location.href = "https://www.linkedin.com/in/diego-ferreira-34b6348b/";
+}
+
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto")
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("pagina carregada");
+}
+
+function funcaochange(elemento){
+    console.log(elemento.value);
+}
+
+
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+
+var idade = prompt ("Qual sua idade");
+console.log(validaIdade(idade));
+*/
+
+
+/*
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+alert(soma(5, 10));
+alert (setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+
+/*
 var d = new Date;
 alert(d);
 alert("Hoje é dia " + d.getDay() + " / " + d.getMonth() + " / " + d.getFullYear());
 alert("São " + d.getHours() + " horas");
 alert("e " + d.getMinutes() + " minutos");
-
+*/
 
 /*
 var count;
